63fdfd65707d854bc172abef41c4b44f388654c0
Keshav Jena <keshav29903@gmail.com>
2024-02-07 19:15:03 +0530

    Footer and Responsiveness

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'; 
 
 import './Footer.scss'
+import logo from '../../assets/ecocircuit_logo2-removebg.png'
 
 
 const Footer = () => {
   return (
     <>
-        Footer
+        <div className='footer'>
+          <div className='footer-logo'>
+            <img src={logo} alt="" />
+          </div>
+
+          <div className="footer-links">
+            <ul className="footer__list">
+            <li className="footer__item">
+              <Link to="/" className="footer__link">
+                Home
+              </Link>
+            </li>
+            <li className="footer__item">
+              <Link to="/about" className="footer__link">
+                About
+              </Link>
+            </li>
+            <li className="footer__item">
+              <Link to="/blogs" className="footer__link">
+                Blogs
+              </Link>
+            </li>
+            <li className="footer__item">
+              <Link to="/contact" className="footer__link">
+                Contact
+              </Link>
+            </li>
+            </ul>
+          </div>
+
+          <div className='footer-newsletter'>
+            <input type='email' placeholder='Enter your email' />
+            <button>Subscribe</button>
+          </div>
+        </div>
     </>
   )
 }
