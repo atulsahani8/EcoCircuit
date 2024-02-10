8d56cb1d583b2d52082ab5c6ae74f66d3dc87fc7
atulsahani8 <atulsahani8888@gmail.com>
2024-02-10 10:58:31 +0530

    blogs page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer'; 
-import { Header, Login, Register, Blogs, Contact, About} from './Containers'
-
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import { Header, Login, Register, Blogs, Contact, About } from "./Containers";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Header />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/blogs" element={<Blogs/>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/Containers/Blogs/Blogs.jsx
+++ frontend/src/Containers/Blogs/Blogs.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import BlogCard from "../..//Components/BlogCard/BlogCard";
+import "./Blogs.scss";
+import img1 from "../../assets/BG.jpg";
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="blog-container">
+      <BlogCard
+        title="Lund iguana"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageUrl={img1}
+      />
+      <BlogCard
+        title="Lund iguana"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageUrl={img1}
+      />
+      <BlogCard
+        title="Lund iguana"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        imageUrl={img1}
+      />
+    </div>
+  );
+};
 
-export default Blogs
+export default Blogs;
